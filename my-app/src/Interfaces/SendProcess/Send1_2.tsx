8f6bb461785b14ffd1b6889cc29536cdf1f470f5
inthhh <glass005533@gmail.com>
2023-11-20 01:11:14 +0900

    feat: 연락처, 계좌 api 연동

--- my-app/src/Interfaces/SendProcess/Send1_2.tsx
+++ my-app/src/Interfaces/SendProcess/Send1_2.tsx
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../Main.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setOption } from "../../redux/store"; // setOption import 추가
 
+interface Number {
+  toCustomerId: number;
+  accountNumber: string;
+  bankCode: string;
+  phoneNumber: string;
+  numberName: string;
+}
+
+// 연락처 조회 함수
 function Send1_2() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   // const [selectedOption, setSelectedOption] = useState(null);
   const selectedOption = useSelector((state: any) => state.selectedOption);
+
+  const [numbers, setNumbers] = useState<Number[]>([]);
+  useEffect(() => {
+    fetch("https://with-pet-be.org/api/numbers")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.code === 200) {
+          setNumbers(data.data);
+          console.log(data.data);
+        } else {
+          console.error("Error fetching accounts:", data.message);
+        }
+      })
+      .catch((error) => {
+        // Handle network error
+        console.error("Network error:", error);
+      });
+  }, []);
+
   const handleToBefore = () => {
     navigate("/Send1_1");
   };
@@ -29,6 +57,13 @@ function Send1_2() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <div>연락처</div>
+      {numbers.map((number, index) => (
+        <div key={index} className="send-box" style={{ height: "150px", marginBottom: "10px" }}>
+          {number.numberName}
+          <br />
+          {number.phoneNumber}
+        </div>
+      ))}
       <div className="buttonContainer">
         <div className="beforebtn" onClick={handleToBefore}>
           &lt; 이전
--- my-app/src/Interfaces/SendProcess/SendSecond.tsx
+++ my-app/src/Interfaces/SendProcess/SendSecond.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../Main.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setOption } from "../../redux/store"; // setOption import 추가
 
+interface Account {
+  accountCode: string;
+  accountId: number;
+  accountNumber: string;
+  balance: number;
+}
+
 function SendSecond() {
-  const [selectedAccount, setSelectedAccount] = useState(null);
+  const [selectedAccount, setSelectedAccount] = useState<Account>();
   const selectedOption = useSelector((state: any) => state.selectedOption);
   const navigate = useNavigate();
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  // const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("https://with-pet-be.org/api/accounts")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.code === 200) {
+          setAccounts(data.data.accountResponses);
+          console.log(data.data.accountResponses);
+        } else {
+          console.error("Error fetching accounts:", data.message);
+        }
+      })
+      .catch((error) => {
+        // Handle network error
+        console.error("Network error:", error);
+      });
+  }, []);
+
   const handleToBefore = () => {
     navigate("/SendFirst");
   };
@@ -16,14 +43,15 @@ function SendSecond() {
     else navigate("/Send2_1");
   };
   const handleAccountClick = (account: any) => {
+    console.log(selectedAccount);
     setSelectedAccount(account);
   };
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <div>[{selectedOption}로 송금하기]</div>
       <div style={{ padding: "5px", fontWeight: "bold" }}>나의 전체계좌</div>
-      <div>내 계좌 : 3개 </div>
-      <div
+      <div>내 계좌 : {accounts.length}개 </div>
+      {/* <div
         className={`send-box ${selectedAccount === 1 ? "selected" : ""}`}
         style={{ height: "150px", marginBottom: "10px" }}
         onClick={() => handleAccountClick(1)}
@@ -33,29 +61,21 @@ function SendSecond() {
         000-000000-00000
         <br />
         잔액 : 100,000,000원
-      </div>
-      <div
-        className={`send-box ${selectedAccount === 2 ? "selected" : ""}`}
-        style={{ height: "150px", marginBottom: "10px" }}
-        onClick={() => handleAccountClick(2)}
-      >
-        입출금
-        <br />
-        000-000000-00000
-        <br />
-        잔액 : 100,000,000원
-      </div>
-      <div
-        className={`send-box ${selectedAccount === 3 ? "selected" : ""}`}
-        style={{ height: "150px", marginBottom: "10px" }}
-        onClick={() => handleAccountClick(3)}
-      >
-        입출금
-        <br />
-        000-000000-00000
-        <br />
-        잔액 : 100,000,000원
-      </div>
+      </div> */}
+      {accounts.map((account) => (
+        <div
+          key={account.accountId}
+          className={`send-box ${selectedAccount && selectedAccount.accountId === account.accountId ? "selected" : ""}`}
+          style={{ height: "150px", marginBottom: "10px" }}
+          onClick={() => handleAccountClick(account)}
+        >
+          {account.accountCode}
+          <br />
+          {account.accountNumber}
+          <br />
+          잔액 : {account.balance.toLocaleString()}원
+        </div>
+      ))}
       <div className="buttonContainer">
         <div className="beforebtn" onClick={handleToBefore}>
           &lt; 이전
