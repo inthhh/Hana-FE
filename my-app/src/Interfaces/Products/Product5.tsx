dd124006ae43f13343c60e4df8dc636311b25261
inthhh <glass005533@gmail.com>
2023-12-12 23:14:27 +0900

    feat: product 3, 4 상세설명/선택 버튼 분리

--- my-app/src/Interfaces/Products/Product5.tsx
+++ my-app/src/Interfaces/Products/Product5.tsx
@@ -71,27 +71,82 @@ function Product5() {
     }
   }, []);
 
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const isSelected = (productName: string) => selectedProduct === productName;
+  const handleSelect1 = () => {
+    setSelectedProduct("1");
+  };
+  const handleSelect2 = () => {
+    setSelectedProduct("2");
+  };
+  const handleSelect3 = () => {
+    setSelectedProduct("3");
+  };
+
   return (
     <div>
       <div style={{ paddingTop: "50px", fontSize: "35px" }}>연금을 받는 것에</div>
       <div style={{ paddingTop: "10px", fontSize: "35px" }}>도움이 될 수 있는</div>
       <div style={{ paddingTop: "10px", fontSize: "35px", marginBottom: "30px" }}> 상품을 추천해드릴게요.</div>
       <img src={p3} width={"100px"} />
-      <div
-        className="products"
-        onClick={() => handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1455928_115157.jsp")}
-      >
-        연금하나 월복리 적금
+      <div className={`products ${isSelected("1") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          연금하나 월복리 적금
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() =>
+                handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1455928_115157.jsp")
+              }
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect1}>
+              선택
+            </div>
+          </div>
+        </div>
       </div>
-      <div
-        className="products"
-        onClick={() => handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080103/1455931_115188.jsp")}
-      >
-        연금하나 통장
+      <div className={`products ${isSelected("2") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          연금하나 통장
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() =>
+                handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080103/1455931_115188.jsp")
+              }
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect2}>
+              선택
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="products" onClick={() => handleLink("https://www.hanaw.com/main/finance/trade/FP_030806_P.cmd")}>
-        <div>
-          연금저축계좌<p style={{ fontSize: "20px", color: "red", marginTop: "5px" }}>원금손실가능성있음</p>
+      <div className={`products ${isSelected("3") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          <div>
+            연금저축계좌<p style={{ fontSize: "20px", color: "red", marginTop: "5px" }}>원금손실가능성있음</p>
+          </div>
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() => handleLink("https://www.hanaw.com/main/finance/trade/FP_030806_P.cmd")}
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect3}>
+              선택
+            </div>
+          </div>
         </div>
       </div>
       <div className="counseling">상담하기</div>
--- my-app/src/Interfaces/Products/Product6.tsx
+++ my-app/src/Interfaces/Products/Product6.tsx
@@ -71,31 +71,85 @@ function Product6() {
     }
   }, []);
 
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const isSelected = (productName: string) => selectedProduct === productName;
+  const handleSelect1 = () => {
+    setSelectedProduct("1");
+  };
+  const handleSelect2 = () => {
+    setSelectedProduct("2");
+  };
+  const handleSelect3 = () => {
+    setSelectedProduct("3");
+  };
+
   return (
     <div>
       <div style={{ paddingTop: "50px", fontSize: "35px" }}>집 구매하는 것에</div>
       <div style={{ paddingTop: "10px", fontSize: "35px" }}>도움이 될 수 있는</div>
       <div style={{ paddingTop: "10px", fontSize: "35px", marginBottom: "30px" }}> 상품을 추천해드릴게요.</div>
       <img src={p4} width={"100px"} />
-      <div
-        className="products"
-        onClick={() => handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1419695_115157.jsp")}
-      >
-        주택청약종합저축
+      <div className={`products ${isSelected("1") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          주택청약종합저축
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() =>
+                handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1419695_115157.jsp")
+              }
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect1}>
+              선택
+            </div>
+          </div>
+        </div>
       </div>
-      <div
-        className="products"
-        onClick={() => handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1450446_115157.jsp")}
-      >
-        내집마련 더블업적금
+      <div className={`products ${isSelected("2") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          내집마련 더블업적금
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() =>
+                handleLink("https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1450446_115157.jsp")
+              }
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect2}>
+              선택
+            </div>
+          </div>
+        </div>
       </div>
-      <div
-        className="products"
-        onClick={() =>
-          handleLink("https://www.kebhana.com/cont/mall/mall08/mall0802/mall080203/1489881_115198.jsp?_menuNo=98786")
-        }
-      >
-        주거안정 주택구입자금대출
+      <div className={`products ${isSelected("3") && "selected"}`}>
+        <div style={{ marginTop: "5px" }}>
+          주거안정 주택구입자금대출
+          <div className="p-horizontal"></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
+            <div
+              style={{ margin: "0 auto" }}
+              onClick={() =>
+                handleLink(
+                  "https://www.kebhana.com/cont/mall/mall08/mall0802/mall080203/1489881_115198.jsp?_menuNo=98786"
+                )
+              }
+            >
+              상세설명 보기
+            </div>
+            <div className="p-vertical"></div>
+            <div style={{ margin: "0 auto", padding: "0 5px" }} onClick={handleSelect3}>
+              선택
+            </div>
+          </div>
+        </div>
       </div>
       <div className="counseling">상담하기</div>
       <div style={{ height: "80px" }}></div> {/* 스크롤 마진 영역 */}
